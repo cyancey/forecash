91e65d6e963c70086c6118ab0f9e6d050ad84c26
Chris Yancey <cyancey1@gmail.com>
2014-06-21 13:28:48 -0700

    initial commit

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+Forecash::Application.config.secret_token = '2b4de35166f11425f95156158d1f20180cbb7054424f06cee6f1e804239d33383320ea0e020eecc8cca926da24aeed55773614580cc7b53d0ddee72e89b59284'
