77d77cd0314cce2b851e4342acc15696865f4450
Nick Meehan <nnmeehan@gmail.com>
2014-06-21 14:40:18 -0700

    Migrated, and rolled back migration.

--- db/schema.rb
+++ db/schema.rb
@@ -11,16 +11,6 @@
 #
 # It's strongly recommended to check this file into your version control system.
 
-ActiveRecord::Schema.define(:version => 20140621205215) do
-
-  create_table "transactions", :force => true do |t|
-    t.text     "description"
-    t.float    "amount"
-    t.string   "frequency"
-    t.integer  "number_of_transactions"
-    t.date     "first_transaction_date"
-    t.datetime "created_at",             :null => false
-    t.datetime "updated_at",             :null => false
-  end
+ActiveRecord::Schema.define(:version => 0) do
 
 end
