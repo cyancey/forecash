b3b60b2befd904ddb51f0b9fc4c22bd4df980161
Nick Meehan <nnmeehan@gmail.com>
2014-06-22 00:55:47 -0700

    Added destroy route for transaction removal.

--- app/controllers/transactions_controller.rb
+++ app/controllers/transactions_controller.rb
@@ -5,8 +5,17 @@ class TransactionsController < ApplicationController
 
 	def create # get to Frequency if have time, may have to delete frequency
 		p params
-		@transaction = Transaction.create(description: params[:description], amount: params[:number], 
+		@transaction = Transaction.create(description: params[:description], amount: params[:number],
 			cash_inflow: Transaction.inflow_converter(params[:cash_flow]), date: params[:date])
 		render json: @transaction
 	end
+
+  def destroy
+    @transaction = Transaction.find(params[:id])
+    if @transaction.destroy
+      render json: Transaction.all
+    else
+      render json: { errors: @transaction.errors.full_messages }
+    end
+  end
 end
