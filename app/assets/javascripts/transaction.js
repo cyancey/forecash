77e532b6cd85d3e58ed154a2e71918443a106c98
Chris Yancey <cyancey1@gmail.com>
2014-06-21 18:06:10 -0700

    added date comparison and increment functions to Scenario model

--- app/assets/javascripts/transaction.js
+++ app/assets/javascripts/transaction.js
@@ -15,5 +15,30 @@ function Scenario(args){
 Scenario.prototype = {
   push: function(transaction){
     this.transactions.push(transaction)
+  },
+
+  buildChartData: function() {
+    var balances = []
+    var date = new Date(Date.now())
+  },
+
+  dateMatch: function(dateOne, dateTwo) {
+    var date1 = this.sanatizeDate(new Date(Date.parse(dateOne)))
+    var date2 = this.sanatizeDate(new Date(Date.parse(dateTwo)))
+    return date1.getTime() === date2.getTime()
+  },
+
+  incrementDay: function(dateObj) {
+    var month = dateObj.getMonth()
+    var day = dateObj.getDate()
+    var year = dateObj.getFullYear()
+    return new Date(year, month, day+1)
+  },
+
+  sanatizeDate: function(dateObj) {
+    var month = dateObj.getMonth()
+    var day = dateObj.getDate()
+    var year = dateObj.getFullYear()
+    return new Date(year, month, day)
   }
 }
