4c904483f42cefb55b72c308b471a6ef941d4271
Andrew Fruth <anfruth@gmail.com>
2014-06-22 00:14:29 -0700

    Back to yearly chart

--- app/assets/javascripts/application_controller.js
+++ app/assets/javascripts/application_controller.js
@@ -14,7 +14,7 @@ ApplicationController.prototype = {
     this.scenario.update()
   },
   update: function(transactions) {
-    var dataSet = DataMonger.transactionsToChartData(transactions, 30, 0)
+    var dataSet = DataMonger.transactionsToChartData(transactions, 365, 0)
     this.chartController.drawChart(dataSet)
     List.update(this.scenario)
   }
