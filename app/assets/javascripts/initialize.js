0cb515a6960fb64224788f3b179928fa32e599e8
Nick Meehan <nnmeehan@gmail.com>
2014-06-21 20:22:03 -0700

    Added styling to highcharts chart.

--- app/assets/javascripts/initialize.js
+++ app/assets/javascripts/initialize.js
@@ -8,20 +8,42 @@ var options = {
             type: 'spline'
         },
         title: {
-            text: 'Fruit Consumption'
+            text: ''
+        },
+        legend: {
+            enabled: false
         },
         xAxis: {
+            title: {
+              text: 'Dates'
+            },
             type: 'datetime'
             // categories: ['Apples', 'Bananas', 'Oranges']
         },
         yAxis: {
             title: {
-                text: 'Fruit eaten'
-            }
+                text: 'Cashflow'
+            },
+            floor: -100000,
+            ceiling: 100000,
+            plotLines: [{
+                          color: '#000000',
+                          width: 3,
+                          value: 0
+                        }]
         },
         series: [{
-            data: [29.9, 71.5, 106.4, 129.2, 144.0, 176.0, 135.6, 148.5, 216.4, 194.1, 95.6, 54.4],
-            pointStart: Date.UTC(2010, 0, 1),
-            pointInterval: 24 * 3600 * 1000 // one day
-        }]
+            name: false,
+            data: [29.9, 71.5, 106.4, 129.2, -144.0, 176.0, 135.6, 148.5, 216.4, 194.1, 95.6, 54.4],
+            pointStart: Date.now(),
+            pointInterval: 24 * 3600 * 1000, // one day
+            color: '#00E000'
+        }],
+        tooltip: {
+          backgroundColor: '#00E000',
+          formatter: function() {
+
+                          return '<b>$' + this.y.toFixed(2) +'</b>';
+                      }
+        }
     }
